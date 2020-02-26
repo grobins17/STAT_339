7025614a0d78750315164cd96b49714eea0c1cd4
grobins17 <61026632+grobins17@users.noreply.github.com>
2020-02-25 23:38:03 -0500

    Add files via upload

--- HW1b/HW1bcode.py
+++ HW1b/HW1bcode.py
@@ -55,7 +55,7 @@ def kfoldvalidation(data, K = 10, seed = 1, D= 1, lambd = 0, trainingerror = Fal
         return stats.mean(validation_error), stats.stdev(validation_error), stats.mean(training_error), stats.stdev(training_error)
     else:
         return stats.mean(validation_error), stats.stdev(validation_error)
-def polychoose(t, x, D, K, seed =1, training_error = False): #TODO: training error 
+def polychoose(t, x, D, K, seed =1, training_error = False):  
     t = np.vstack(t)
     x = np.vstack(x)
     data = np.append(x, t, axis =1)
@@ -74,90 +74,120 @@ def polychoose(t, x, D, K, seed =1, training_error = False): #TODO: training err
         return fold_means, fold_sds, training_means, training_sds, fold_means.index(min(fold_means)) +1 
     else:
          return fold_means, fold_sds, fold_means.index(min(fold_means)) +1 
+def gridsearch(data, D, seed = 1):
+    mse = np.zeros((D, 20))
+    for l in range(20):
+        for i in range(D):
+            mse[i , l] = kfoldvalidation(data, 10, seed, i+1, l)[0]
+    indices = np.where(mse == np.amin(mse))
+    print(indices[0], indices[1])
+    print(mse[indices[0], indices[1]])
+    return indices[0], indices[1]
+def truetestset(t, pred, D, lambd = 0):
+    se2012 = []
+    se2016 = []
+    for i in range(D):
+        w = polyregression(t, pred, i+1, lambd)
+        twelve = y((2012 - Year_mean)/Year_sd, w)
+        sixteen = y((2016 - Year_mean)/Year_sd, w)
+        se2012.append(math.pow((-1.09) - twelve, 2))
+        se2016.append(math.pow((-1.18) - sixteen, 2))
+    return se2012, se2016
+
 A = np.loadtxt("womens100.csv", delimiter = ",")
+global Year_mean
 Year_mean = stats.mean(A[:,0])
+global Year_sd
 Year_sd = stats.stdev(A[:, 0])
+global time_sd 
 time_sd = stats.stdev(A[:, 1])
+global time_mean 
 time_mean = stats.mean(A[:, 1])
 A = sci.zscore(A, axis = 0)
-
 w = polyregression(np.vstack(A[:, 0]), np.vstack(A[:,1]), 1)
 #same as the one in the textbook
 x = np.linspace(-2, 2, 10000)
 plt.scatter(A[:,0], A[:, 1])
 plt.plot(x, y(x,w), color = "red")
+plt.xlabel("Standardized Years")
+plt.ylabel("Standardized Times")
+plt.title("Olympic Times vs. Years")
 plt.show()
 x = y((2012 - Year_mean)/Year_sd, w)
 #print((x * time_sd)+ time_mean)
-##10.5996
-##real was 10.75
+##10.5996 
+##real was 10.75 -> z score of -1.0901552042195117
 x = y((2016 - Year_mean)/Year_sd, w)
 #print((x * time_sd)+ time_mean)
 ##10.5393
-##real was 10.71
-#TODO: calculate the squared error
+##real was 10.71 -> z score of -1.1826237260059862
+
 B = np.loadtxt("synthdata2016.csv", delimiter = ",")
 B = sci.zscore(B, axis = 0)
 w = polyregression(np.vstack(B[:,1]), np.vstack(B[:,0]), 4)
 plt.cla()
 plt.scatter(B[:, 0], B[:, 1])
 x = np.linspace(-1.75, 1.75, 1000)
+plt.xlabel("Standardized X")
+plt.ylabel("Standardized Y")
+plt.title("Fitted Synthetic Data")
 plt.plot(x, y(x, w))
 plt.show()
-
-print(kfoldvalidation(B, 10, 1,3, 0, True))
-
-
 
 f = polychoose(B[:,1], B[:,0], 10, 50, 1, True)
 plt.cla()
 x = np.linspace(1, len(f[0]), len(f[0]))
 plt.title("Optimal Polynomial for Synthetic Data, K = 50")
-plt.plot(x, f[0], color = "blue", label = "Validation Error")
-plt.plot(x, f[2], color = "red", label = "Training Error")
-plt.errorbar(x, f[0], f[1], color = "blue")
-plt.errorbar(x, f[2], f[3], color = "red")
+plt.errorbar(x, f[0], f[1], ecolor = "black", color = "blue", label = "Validation Error")
+plt.errorbar(x, f[2], f[3], ecolor = "magenta", color = "red", label = "Training Error")
 plt.legend()
 plt.ylabel("MSE")
 plt.xlabel("Polynomial Order")
-plt.show()
+plt.show() #optimal polynomial is 4th order
 
 f = polychoose(B[:,1], B[:,0], 10, 10, 1, True)
 plt.cla()
 x = np.linspace(1, len(f[0]), len(f[0]))
 plt.title("Optimal Polynomial for Synthetic Data, K = 10")
-plt.plot(x, f[0], color = "blue", label = "Validation Error")
-plt.plot(x, f[2], color = "red", label = "Training Error")
-plt.errorbar(x, f[0], f[1], color = "blue")
-plt.errorbar(x, f[2], f[3], color = "red")
+plt.errorbar(x, f[0], f[1], ecolor = "black", color = "blue", label = "Validation Error")
+plt.errorbar(x, f[2], f[3], ecolor = "magenta", color = "red", label = "Training Error")
 plt.legend()
 plt.ylabel("MSE")
 plt.xlabel("Polynomial Order")
-plt.show()
+plt.show() #optimal is still 4th order
 
 
 f = polychoose(A[:,1], A[:,0], 6, 10, 1, True)
 plt.cla()
 x = np.linspace(1, len(f[0]), len(f[0]))
 plt.title("Optimal Polynomial for Olympic Data, K = 10")
-plt.plot(x, f[0], color = "c", label = "Validation Error")
-plt.plot(x, f[2], color = "r", label = "Training Error")
-plt.errorbar(x, f[0], f[1], ecolor = "blue")
-plt.errorbar(x, f[2], f[3], ecolor = "blue")
+plt.errorbar(x, f[0], f[1], ecolor = "black", color = "blue", label = "Validation Error")
+plt.errorbar(x, f[2], f[3], ecolor = "magenta", color = "red", label = "Training Error")
 plt.legend()
 plt.ylabel("MSE")
 plt.xlabel("Polynomial Order")
-plt.show()
+plt.show() #second order poly
 
 f = polychoose(A[:,1], A[:,0], 6, 19, 1, True)
 plt.cla()
 x = np.linspace(1, len(f[0]), len(f[0]))
 plt.title("Optimal Polynomial for Olympic Data, K = 19")
-plt.plot(x, f[0], color = "c", label = "Validation Error")
-plt.plot(x, f[2], color = "red", label = "Training Error")
-plt.errorbar(x, f[0], f[1], ecolor = "blue")
-plt.errorbar(x, f[2], f[3], ecolor = "orange")
+#plt.plot(x, f[0], color = "c", label = "Validation Error")
+#plt.plot(x, f[2], color = "red", label = "Training Error")
+plt.errorbar(x, f[0], f[1], ecolor = "black", color = "blue", label = "Validation Error")
+plt.errorbar(x, f[2], f[3], ecolor = "magenta", color = "red", label = "Training Error")
 plt.legend()
 plt.ylabel("MSE")
 plt.xlabel("Polynomial Order")
-plt.show()
+plt.show() #second order poly
+
+gridsearch(A, 20) #optimal is reached with 1st order with lambda = 2 
+gridsearch(B, 20) #optimal is reached with 4th order polnomial with lambda = 0
+
+b, c = truetestset(np.vstack(A[:, 1]), np.vstack(A[:,0]), D = 10)
+print(b.index(min(b))+1, min(b)) #suggests a 6th order poly
+print(c.index(min(c))+1, min(c)) #suggests a 2nd order poly
+
+d, e = truetestset(np.vstack(A[:, 1]), np.vstack(A[:,0]), 1, 2)
+print(d, e) #our suggested model does better!
+
